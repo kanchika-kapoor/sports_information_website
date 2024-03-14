2a6d202bf16990f7a4ce8d516fc84dd4d11af29f
kanchika-kapoor <kanchika.ai@gmail.com>
2024-03-14 21:50:57 +0530

    added main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,20 @@
 
 export default function Home() {
+  const nd = new Date()
+  const dateConvert = nd.toDateString()
+
   return (
-    <main className="flex text-textPrimary">
-      Hello world
-    </main>
+    <section className="px-2 md:px-4 md:w-[600px]">
+      <div className="flex justify-between items-center mb-4 md:mb-2">
+        <h1 className="text-md md:text-xl font-bold">
+          MATCHES
+        </h1>
+        <div className="px-4 py-0 md:py-1 bg-slate-600
+        rounded-md text-textPrimary text-sm">
+          <p>{dateConvert}</p>
+        </div>
+      </div>
+      {/* match data */}
+    </section>
   );
 }
