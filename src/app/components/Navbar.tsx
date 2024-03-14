2d4247c0122ce9c49593b2e721a556d29e745485
kanchika-kapoor <kanchika.ai@gmail.com>
2024-03-14 20:49:22 +0530

    added navbar

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,8 +1,18 @@
-import React from "react";
+import React from "react"
+import Link from "next/link"
+import Image from "next/image"
 
 const Navbar = ()=>{
     return (
-        <div>Navbar</div>
+        <div className="flex justify-between items-center py-2">
+            <Link href='/' className="flex items-center space-x-2">
+                <div className="relative w-[30px] h-[30px]">
+                    <Image src="/logoipsum.svg" alt="logo" fill
+                    className="object-cover"/>
+                </div>
+                <span className="text-2xl font-bold non md:block">Football News</span>
+            </Link>
+        </div>
     )
 }
 
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
+import Navbar from "./components/Navbar";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -18,7 +20,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <main className="px-2 md:px-16 md:py-2 text-textPrimary">
-          {/* navbar*/}
+          <Navbar/>
           <section className="flex space-x-4">
             {/*sidebar*/}
             {children}
